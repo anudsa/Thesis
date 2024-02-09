cf4a5c5ca779e0dbff730c1212ec43a399f2cd2f
anudsa <133996511+anudsa@users.noreply.github.com>
2024-02-08 20:01:09 -0600

    Update 3sensors.py

--- Code/3sensors.py
+++ Code/3sensors.py
@@ -4,27 +4,26 @@ import time
 import os
 import re
 import RPi.GPIO as GPIO
+
 #function to get temperatures
 def getTemperatures():
-    # returns a list of temperatures from all available "28*" onewire devices
     allTemps = list()
     onewire_basedir = "/sys/bus/w1/devices/"
     onewire_devices = os.listdir(onewire_basedir)
     onewire_retemp = re.compile('t=(\d*)')
-    #Loop to find if the listed device is a sensor i.e. name starts with 28
+
     for device_string in onewire_devices:
         if device_string.startswith("28"):
             onewire_path = os.path.join(onewire_basedir, device_string, "w1_slave")
             onewire_devfile = open(onewire_path, "r")
             onewire_devtext = onewire_devfile.readlines()
             onewire_temp = onewire_retemp.search(onewire_devtext[1])
-            #Conversion to display temp in C°
             temperature = int(onewire_temp.group(1)) / 1000.0
             allTemps.append(temperature)
+
     return allTemps
 
 def read_line(sensor):
-    # Function to read a line from the sensor
     lsl = len(b'\r')
     line_buffer = []
     while True:
@@ -37,7 +36,6 @@ def read_line(sensor):
     return b''.join(line_buffer)
 
 def read_lines(sensor):
-    # Function to read multiple lines from the sensor
     lines = []
     try:
         while True:
@@ -52,7 +50,6 @@ def read_lines(sensor):
         return None
 
 def send_cmd(sensor, cmd):
-    # Function to send a command to the Atlas Sensor
     buf = cmd + "\r"
     try:
         sensor.write(buf.encode('utf-8'))
@@ -65,23 +62,19 @@ def poll_sensors(sensor1, sensor2, usbport1, usbport2):
     print("Polling sensors connected to {} and {}: ".format(usbport1, usbport2))
 
     while True:
-        # Read temperatures from DS18B20 one-wire sensor
         one_wire_temps = getTemperatures()
 
-        # Print temperatures from the one-wire sensor
         if one_wire_temps:
             print("Temperature from One-Wire Sensor: {:.2f}°C".format(one_wire_temps[0]))
         else:
             print("Unable to read temperature from One-Wire Sensor.")
 
-        # Poll the first sensor
         send_cmd(sensor1, "R")
         lines1 = read_lines(sensor1)
         for i in range(len(lines1)):
             if lines1[i][0] != b'*'[0]:
                 print("Response from {}: {}".format(usbport1, lines1[i].decode('utf-8')))
 
-        # Poll the second sensor
         send_cmd(sensor2, "R")
         lines2 = read_lines(sensor2)
         for i in range(len(lines2)):
@@ -90,8 +83,19 @@ def poll_sensors(sensor1, sensor2, usbport1, usbport2):
 
         time.sleep(1)  # Adjust as needed
 
-
-
 if __name__ == "__main__":
+    usbports = ['/dev/ttyUSB0', '/dev/ttyUSB1']
+
+    try:
+        sensor1 = serial.Serial(usbports[0], 9600, timeout=0)
+    except serial.SerialException as e:
+        print("Error opening serial port {}: {}".format(usbports[0], e))
+        sys.exit(1)
+
+    try:
+        sensor2 = serial.Serial(usbports[1], 9600, timeout=0)
+    except serial.SerialException as e:
+        print("Error opening serial port {}: {}".format(usbports[1], e))
+        sys.exit(1)
 
     poll_sensors(sensor1, sensor2, usbports[0], usbports[1])
