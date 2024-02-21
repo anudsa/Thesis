0568189bb9ef0497cba40bf841b763fdf44aa764
Anu <anu@MacBook-Air.local>
2024-02-20 22:49:31 -0600

    collation problem patched

--- sensores.sql
+++ sensores.sql
@@ -30,7 +30,7 @@ CREATE TABLE `lecturas` (
   `conductividad_eléctrica` decimal(5,2) DEFAULT NULL,
   `oxígeno_disuelto` decimal(5,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
