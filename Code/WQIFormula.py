5ff7e4c2e14c5d7588784ebf623691b5fa6b5598
anudsa <anwardelgadosilva@gmail.com>
2024-03-24 20:19:21 -0600

    WQI formula ranges updated

--- Code/WQIFormula.py
+++ Code/WQIFormula.py
@@ -12,8 +12,8 @@ oxigenoDisuelto=6
 def parametrizacion(conductividad,temp,pH,oxigenoDisuelto):
     #Creamos una lista vacía de 4 elementos para P y los pesos fijos para W.
     P=[None]*4
-    W=[1,2,3,4]
-    if conductividad < 250 or conductividad > 700:
+
+    if conductividad < 250 or conductividad > 500:
         P[1]=2
     else:
         P[1]=1
@@ -23,12 +23,12 @@ def parametrizacion(conductividad,temp,pH,oxigenoDisuelto):
     else:
         P[0]=1
 
-    if pH < 6 or pH > 8.5:
+    if pH < 6.5 or pH > 8.5:
         P[2]=2
     else:
         P[2]=1
 
-    if oxigenoDisuelto < 5 or oxigenoDisuelto > 10:
+    if oxigenoDisuelto < 5 or oxigenoDisuelto > 7:
         P[3]=2
     else:
         P[3]=1
@@ -40,9 +40,11 @@ def calculo(P,W):
     Denominador = sum(P) #Suma los elementos de la lista P
     WQI=Numerador/Denominador
     return WQI
-
+# The calculated WQIs were then classified according to
+# the following range: >2.5 were excellent quality water;
+# 2.0 to 2.5 good quality water; and <2.0 poor quality water. 
 def interpretacion(WQI):
-    if WQI < 1.5:
+    if WQI < 2:
         return "Calidad Baja"
     elif WQI <2.5:
         return "Calidad Aceptable"
@@ -50,6 +52,7 @@ def interpretacion(WQI):
         return "Calidad Excelente"
 
 if __name__ == "__main__":
+    W=[1,2,3,4]
     P=parametrizacion(conductividad,temp,pH,oxigenoDisuelto)
     WQI = calculo(P,W)
     print(WQI)
