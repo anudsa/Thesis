5ce66fa297e925da56910591a2f7292b90fa63ab
Anu <anu@MacBook-Air.local>
2024-04-28 16:06:08 -0600

    intervalos tests

--- Code/guiSpaces.py
+++ Code/guiSpaces.py
@@ -451,15 +451,18 @@ if __name__ == "__main__":
             return
         global detener_intervalos
         detener_intervalos=False
+        offset_inicial=0
         tiempo_transcurrido = 0
         tiempo_inicio = time.time()
         for i in range(duracion_intervalos // tiempo_muestreo_intervalos +1):
             actualizarDatosIntervalos()
             tiempo_transcurrido = time.time() - tiempo_inicio
             print("Tiempo transcurrido:", tiempo_transcurrido)
+            if  tiempo_transcurrido<=0.9:
+                offset_inicial=tiempo_transcurrido
             if  detener_intervalos==True:
                 break
-            time.sleep(tiempo_muestreo_intervalos-0.8)
+            time.sleep(tiempo_muestreo_intervalos-offset_inicial)
         print("Tiempo total:", tiempo_transcurrido)
         messagebox.showinfo("Listo","El intervalo ha terminado.")
 
