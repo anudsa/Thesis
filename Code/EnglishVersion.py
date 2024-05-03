046c57da03a41bef3ce3655bc725f0753ba52d15
Anu <anu@MacBook-Air.local>
2024-05-03 17:04:18 -0600

    English Version

--- Code/EnglishVersion.py
+++ Code/EnglishVersion.py
@@ -1133,7 +1133,7 @@ if __name__ == "__main__":
     )
     #Header text
     canvasPuntual.create_text(
-        399.0,
+        450.0,
         28.0,
         anchor="nw",
         text="Single Measurement",
