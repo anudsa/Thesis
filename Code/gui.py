09b2a847c72ae35202e0fa2788086cae7c027664
Anu <anu@MacBook-Air.local>
2024-05-16 12:11:39 -0600

    Window size changed

--- Code/gui.py
+++ Code/gui.py
@@ -270,7 +270,7 @@ if __name__ == "__main__":
 	
     window = Tk()
     window.title("Sistema de Adquisición de Datos")
-    window.geometry("853x600")
+    window.geometry("1024x600")
     
     
     medicionContinua = Frame(window)
