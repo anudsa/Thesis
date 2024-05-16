f35bc24cc3afa2d9cb15c6edf1b5eadc44fa9aa0
Anu <anu@MacBook-Air.local>
2024-05-16 12:09:00 -0600

    Window size changed

--- Code/gui.py
+++ Code/gui.py
@@ -270,7 +270,7 @@ if __name__ == "__main__":
 	
     window = Tk()
     window.title("Sistema de Adquisición de Datos")
-    window.geometry("768x450")
+    window.geometry("853x500")
     
     
     medicionContinua = Frame(window)
@@ -1236,5 +1236,5 @@ if __name__ == "__main__":
         font=("NunitoSans Regular", 40 * -1)
     )
     
-    #window.resizable(False, False)
+    window.resizable(False, False)
     window.mainloop()
