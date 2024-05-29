babb43a7a436a966c4df994a81562cb8021bdb94
Anu <anu@MacBook-Air.local>
2024-05-28 19:51:11 -0600

    minor changes

--- Code/Formula.py
+++ Code/Formula.py
@@ -1,13 +1,13 @@
 """
 ICA = (Σᵢ₌₁ⁿ Pᵢ×Wᵢ)
       _____________
-     (Σᵢ₌₁ⁿ Pᵢ)
+     (Σᵢ₌₁ⁿ Wᵢ)
 """
 def calcular_subindice_temperatura(T):
     # Subíndice de Temperatura (ICA_T)
     if T > 25:
         subindice_T = 0.1
-    elif 20 <= T <= 25:
+    elif 20 <= T <= 27:
         subindice_T = 1.0
     else:
         subindice_T = 0.5
@@ -27,9 +27,9 @@ def calcular_subindice_ph(pH):
 
 def calcular_subindice_conductividad(CE):
     # Subíndice de Conductividad Eléctrica (ICA_CE)
-    if CE > 0:
+    if CE > 0 and CE < 250:
         subindice_CE = 0.1
-    elif 400 <= CE <= 600:
+    elif 2500 <= CE <= 600:
         subindice_CE = 1.0
     else:
         subindice_CE = 0.5
@@ -39,6 +39,7 @@ def calcular_subindice_conductividad(CE):
 def calcular_indice( CE,T, pH,):
     # Cálculo de los subíndices
     subindice_CE = calcular_subindice_conductividad(CE)
+    print(subindice_CE)
     subindice_T = calcular_subindice_temperatura(T)
     subindice_pH = calcular_subindice_ph(pH)
     # Cálculo del ICA global
@@ -46,31 +47,38 @@ def calcular_indice( CE,T, pH,):
     return indice
 
 def interpretacion(indice):
-    if indice < 0.3:
+    if indice < 0.25:
         return "Baja"
-    elif 0.3 <= indice <= 0.7:
+    elif 0.25 <= indice <= 0.75:
         return "Regular"
     else:
         return "Excelente"
-    
-#For the English version    
-def interpretation(indice):
-    if indice < 0.3:
-        return "Low"
-    elif 0.3 <= indice <= 0.7:
-        return "Regular"
-    else:
-        return "Excellent"
-    
 
 if __name__ == "__main__":
     # Escenarios de prueba
     escenarios = [
-        {'CE': 120,'T': 30, 'pH': 5, },    # Baja
-        { 'CE': 800,'T': 15, 'pH': 8,},      # Aceptable
-        { 'CE': 500,'T': 23, 'pH': 7.6,},   # Excelente
+        {'CE': 120, 'T': 30, 'pH': 5},    # Baja
+        {'CE': 800, 'T': 15, 'pH': 8},     # Aceptable
+        {'CE': 500, 'T': 23, 'pH': 7.6},   # Excelente
     ]
-    for i, escenario in enumerate(escenarios, start=1):
-        ica = calcular_indice(escenario['T'], escenario['pH'], escenario['CE'])
-        interpretacionICA = interpretacion(ica)
-        print(f"Prueba {i}: Índice de Calidad del Agua (ICA) para el escenario {escenario}: {ica:.2f} ({interpretacionICA})")
+
+    # Ejecutar escenarios de prueba
+    for escenario in escenarios:
+        CE = escenario['CE']
+        T = escenario['T']
+        pH = escenario['pH']
+        
+        indice = calcular_indice(CE, T, pH)
+        interpretVariable = interpretacion(indice)
+        
+        print(f"Para CE: {CE}, T: {T}, pH: {pH}, el índice es: {indice} ({interpretVariable})")
+
+#For the English version    
+def interpretation(indice):
+    if indice < 0.25:
+        return "Low"
+    elif 0.25 <= indice <= 0.75:
+        return "Regular"
+    else:
+        return "Excellent"
+    
