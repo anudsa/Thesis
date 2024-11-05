6fa37bac76f9b6a2fc813145a5b2f3e03b24cb18
anudsa <133996511+anudsa@users.noreply.github.com>
2024-11-05 10:55:17 -0600

    Update export.py

--- Code/export.py
+++ Code/export.py
@@ -1,3 +1,5 @@
+
+import config
 import pandas as pd
 import subprocess
 from sqlalchemy import create_engine
@@ -5,7 +7,7 @@ from datetime import datetime
 
 def exportarExcel(id_inicial, id_final):
     # Crea motor de SQLAlchemy
-    engine = create_engine('mysql+mysqlconnector://root:Pa$$w0rd@localhost/Sensores')
+    engine = create_engine(f"mysql+mysqlconnector://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}/{config.DB_NAME}")
 
     # Construye la consulta SQL con el rango de IDs
     consulta_sql = f"SELECT * FROM lecturas WHERE id BETWEEN {id_inicial} AND {id_final}"
@@ -34,7 +36,6 @@ def exportarExcel(id_inicial, id_final):
     nombre_archivo = f"Measurents from {timestampFormat}.xlsx"
 
     # Definir la ruta de la carpeta
-    #ruta_carpeta = "/Users/anu/Documents/IPN/Tesis/Github Repo/Thesis/export/"
     ruta_carpeta = "/home/pi/Desktop/ArchivosExportados/"
     # Guardar el DataFrame en un archivo Excel en la carpeta especificada
     df.to_excel(ruta_carpeta + nombre_archivo, index=False)
